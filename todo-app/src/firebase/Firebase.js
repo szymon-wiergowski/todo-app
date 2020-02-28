76995e91a29aa9ce26f05f7f36c4a536256c8caa
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-02-28 21:30:49 +0100

    add firebase to app

---
+++ todo-app/src/firebase/Firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAdyvU2jhoxIzKcIMmIfHXtK5atQidL6TY",
+  authDomain: "rest-api-jfdz12-sw.firebaseapp.com",
+  databaseURL: "https://rest-api-jfdz12-sw.firebaseio.com",
+  projectId: "rest-api-jfdz12-sw",
+  storageBucket: "rest-api-jfdz12-sw.appspot.com",
+  messagingSenderId: "736124371432",
+  appId: "1:736124371432:web:8bcca6fa7828abd169645d",
+  measurementId: "G-25XJLCNXRE"
+};
+
+export default firebaseConfig;
+
