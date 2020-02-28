db792ac29c568b7a84f5cbb944078966f75244af
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-02-29 00:01:58 +0100

    change ToDoBoard.js to function

--- todo-app/src/components/ToDoBoard.js
+++ todo-app/src/components/ToDoBoard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { List, Segment } from "semantic-ui-react";
 
@@ -7,66 +7,121 @@ import ToDoEditItem from "./ToDoEditItem";
 
 export const BASE_URL = "https://rest-api-jfdz12-sw.firebaseio.com";
 
-export default class ToDoListBoard extends React.Component {
-  state = {
-    editTaskId: null
+const ToDoListBoard = (props) => {
+  const [editTaskId, setEditTaskId] = useState(null);
+
+  const handleOnEdit = markedId => {
+    setEditTaskId(markedId);
   };
 
-  handleOnEdit = markedId => {
-    this.setState({
-      editTaskId: markedId
-    });
+  const handleOnClose = () => {
+    setEditTaskId(null);
   };
 
-  handleOnClose = () => {
-    this.setState({
-      editTaskId: null
-    });
+  const handleOnSave = () => {
+    props.onSave();
+    setEditTaskId(null);
   };
 
-  handleOnSave = () => {
-    this.props.onSave();
-    this.setState({
-      editTaskId: null
-    });
-  };
+  const { todo, onDelete, onCheck } = props;
 
-  render() {
-    const { editTaskId } = this.state;
-    const { todo, onDelete, onCheck } = this.props;
+  return (
+    <Segment>
+      <List divided relaxed size="large">
+        {todo.map(item => {
+          return editTaskId === item.id ? (
+            <ToDoEditItem
+              key={item.id}
+              todo={todo}
+              done={item.done}
+              id={item.id}
+              item={item}
+              onClose={handleOnClose}
+              onSave={handleOnSave}
+              onDelete={onDelete}
+              onCheck={onCheck}
+              onEdit={handleOnEdit}
+            />
+          ) : (
+            <ToDoItem
+              key={item.id}
+              todo={todo}
+              done={item.done}
+              id={item.id}
+              item={item}
+              onDelete={onDelete}
+              onCheck={onCheck}
+              onEdit={handleOnEdit}
+            />
+          );
+        })}
+      </List>
+    </Segment>
+  );
+};
 
-    return (
-      <Segment>
-          <List divided relaxed size="large">
-            {todo.map(item => {
-              return editTaskId === item.id ? (
-                <ToDoEditItem
-                  key={item.id}
-                  todo={todo}
-                  done={item.done}
-                  id={item.id}
-                  item={item}
-                  onClose={this.handleOnClose}
-                  onSave={this.handleOnSave}
-                  onDelete={onDelete}
-                  onCheck={onCheck}
-                  onEdit={this.handleOnEdit}
-                />
-              ) : (
-                <ToDoItem
-                  key={item.id}
-                  todo={todo}
-                  done={item.done}
-                  id={item.id}
-                  item={item}
-                  onDelete={onDelete}
-                  onCheck={onCheck}
-                  onEdit={this.handleOnEdit}
-                />
-              );
-            })}
-          </List>
-      </Segment>
-    );
-  }
-}
+export default ToDoListBoard;
+
+// export default class ToDoListBoard extends React.Component {
+//   state = {
+//     editTaskId: null
+//   };
+
+//   handleOnEdit = markedId => {
+//     this.setState({
+//       editTaskId: markedId
+//     });
+//   };
+
+//   handleOnClose = () => {
+//     this.setState({
+//       editTaskId: null
+//     });
+//   };
+
+//   handleOnSave = () => {
+//     this.props.onSave();
+//     this.setState({
+//       editTaskId: null
+//     });
+//   };
+
+//   render() {
+//     const { editTaskId } = this.state;
+//     const { todo, onDelete, onCheck } = this.props;
+
+//     return (
+//       <Segment>
+//           <List divided relaxed size="large">
+//             {todo.map(item => {
+//               return editTaskId === item.id ? (
+//                 <ToDoEditItem
+//                   key={item.id}
+//                   todo={todo}
+//                   done={item.done}
+//                   id={item.id}
+//                   item={item}
+//                   onClose={this.handleOnClose}
+//                   onSave={this.handleOnSave}
+//                   onDelete={onDelete}
+//                   onCheck={onCheck}
+//                   onEdit={this.handleOnEdit}
+//                 />
+//               ) : (
+//                 <ToDoItem
+//                   key={item.id}
+//                   todo={todo}
+//                   done={item.done}
+//                   id={item.id}
+//                   item={item}
+//                   onDelete={onDelete}
+//                   onCheck={onCheck}
+//                   onEdit={this.handleOnEdit}
+//                 />
+//               );
+//             })}
+//           </List>
+//       </Segment>
+//     );
+//   }
+// }
