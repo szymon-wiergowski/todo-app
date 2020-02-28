be7ba78a5d30667c13dc420603ca55051ffdf313
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-02-28 23:55:30 +0100

    change ToDo.js to function

--- todo-app/src/components/ToDo.js
+++ todo-app/src/components/ToDo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Segment, Dimmer, Loader, Image, Grid } from "semantic-ui-react";
 
@@ -8,14 +8,12 @@ import Auth from "./Auth";
 
 export const BASE_URL = "https://rest-api-jfdz12-sw.firebaseio.com";
 
-export default class ToDo extends React.Component {
-  state = {
-    todo: [],
-    loading: true,
-    error: null
-  };
+const ToDo = () => {
+  const [todo, setTodo] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  fetchToDo = () => {
+  const fetchToDo = () => {
     fetch(`${BASE_URL}/todo.json`)
       .then(res => res.json())
       .then(data => {
@@ -26,67 +24,61 @@ export default class ToDo extends React.Component {
             ...data[key]
           };
         });
-        this.setState({
-          loading: false,
-          todo: formattedData.sort(function(a, b) {
+        setLoading(false);
+        setTodo(
+          formattedData.sort(function(a, b) {
             return b.dateOfCreateTask - a.dateOfCreateTask;
           })
-        });
+        );
       })
-      .catch(err =>
-        this.setState({
-          error: err
-        })
-      );
+      .catch(err => setError(err));
   };
 
-  componentDidMount() {
-    this.fetchToDo();
+  useEffect(() => {
+    fetchToDo();
+  }, []);
+
+  const handleOnAction = () => {
+    fetchToDo();
+  };
+
+  if (error) {
+    return <div>Error: {error}</div>;
   }
 
-  handleOnAction = () => {
-    this.fetchToDo();
-  };
-
-  render() {
-    const { todo, error, loading } = this.state;
-
-    if (error) {
-      return <div>Error: {error}</div>;
-    }
-
-    if (loading) {
-      return (
-        <Segment>
-          <Dimmer active>
-            <Loader size="medium">Loading</Loader>
-          </Dimmer>
-          <Image src="/images/wireframe/short-paragraph.png" />
-        </Segment>
-      );
-    }
+  if (loading) {
     return (
-      <Auth>
-        <Grid
-          textAlign="center"
-          style={{ height: "100vh" }}
-          verticalAlign="middle"
-        >
-          <Segment
-            style={{
-              width: 650
-            }}
-          >
-            <AddTask onAdd={this.handleOnAction} />
-            <ToDoBoard
-              todo={todo}
-              onDelete={this.handleOnAction}
-              onSave={this.handleOnAction}
-              onCheck={this.handleOnAction}
-            />
-          </Segment>
-        </Grid>
-      </Auth>
+      <Segment>
+        <Dimmer active>
+          <Loader size="medium">Loading</Loader>
+        </Dimmer>
+        <Image src="/images/wireframe/short-paragraph.png" />
+      </Segment>
     );
   }
-}
+  return (
+    <Auth>
+      <Grid
+        textAlign="center"
+        style={{ height: "100vh" }}
+        verticalAlign="middle"
+      >
+        <Segment
+          style={{
+            width: 650
+          }}
+        >
+          <AddTask onAdd={handleOnAction} />
+          <ToDoBoard
+            todo={todo}
+            onDelete={handleOnAction}
+            onSave={handleOnAction}
+            onCheck={handleOnAction}
+          />
+        </Segment>
+      </Grid>
+    </Auth>
+  );
+};
+
+export default ToDo;
