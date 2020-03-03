4490bc61d617289c2ed827c4d3120f32cbaaa0ce
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-03-03 21:31:12 +0100

    change ToDOEditItem.js on function component

--- todo-app/src/components/ToDoBoard.js
+++ todo-app/src/components/ToDoBoard.js
@@ -7,7 +7,7 @@ import ToDoEditItem from "./ToDoEditItem";
 
 export const BASE_URL = "https://rest-api-jfdz12-sw.firebaseio.com";
 
-const ToDoListBoard = (props) => {
+const ToDoListBoard = props => {
   const [editTaskId, setEditTaskId] = useState(null);
 
   const handleOnEdit = markedId => {
@@ -61,67 +61,3 @@ const ToDoListBoard = (props) => {
 };
 
 export default ToDoListBoard;
-
-// export default class ToDoListBoard extends React.Component {
-//   state = {
-//     editTaskId: null
-//   };
-
-//   handleOnEdit = markedId => {
-//     this.setState({
-//       editTaskId: markedId
-//     });
-//   };
-
-//   handleOnClose = () => {
-//     this.setState({
-//       editTaskId: null
-//     });
-//   };
-
-//   handleOnSave = () => {
-//     this.props.onSave();
-//     this.setState({
-//       editTaskId: null
-//     });
-//   };
-
-//   render() {
-//     const { editTaskId } = this.state;
-//     const { todo, onDelete, onCheck } = this.props;
-
-//     return (
-//       <Segment>
-//           <List divided relaxed size="large">
-//             {todo.map(item => {
-//               return editTaskId === item.id ? (
-//                 <ToDoEditItem
-//                   key={item.id}
-//                   todo={todo}
-//                   done={item.done}
-//                   id={item.id}
-//                   item={item}
-//                   onClose={this.handleOnClose}
-//                   onSave={this.handleOnSave}
-//                   onDelete={onDelete}
-//                   onCheck={onCheck}
-//                   onEdit={this.handleOnEdit}
-//                 />
-//               ) : (
-//                 <ToDoItem
-//                   key={item.id}
-//                   todo={todo}
-//                   done={item.done}
-//                   id={item.id}
-//                   item={item}
-//                   onDelete={onDelete}
-//                   onCheck={onCheck}
-//                   onEdit={this.handleOnEdit}
-//                 />
-//               );
-//             })}
-//           </List>
-//       </Segment>
-//     );
-//   }
-// }
--- todo-app/src/components/ToDoEditItem.js
+++ todo-app/src/components/ToDoEditItem.js
@@ -1,108 +1,196 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Button, Form, Icon, List, Message, Input } from "semantic-ui-react";
 
 import { BASE_URL } from "./ToDo";
 import Checkbox from "./Checkbox";
 
-export default class ToDoEditList extends React.Component {
-  state = {
-    ...this.props.item,
-    id: null,
-    taskError: false,
-    formError: false
+const ToDoEditList = props => {
+  const { item } = props;
+
+  const [editTask, setEditTask] = useState(item.task);
+  const [taskError, setTaskError] = useState(false);
+  const [formError, setFormError] = useState(false);
+
+  const handleOnChange = event => {
+    setEditTask(event.target.value);
   };
 
-  handleOnChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
-  handleOnSaveClick = event => {
+  const handleOnSaveClick = event => {
     event.preventDefault();
 
     let error = false;
 
-    const taskLength = this.state.task.length;
+    const taskLength = editTask.length;
 
     if (taskLength < 5 || taskLength > 35) {
-      this.setState({ taskError: true });
+      setTaskError(true);
       error = true;
     } else {
-      this.setState({ taskError: false });
+      setTaskError(false);
     }
 
     if (error) {
-      this.setState({ formError: true });
+      setFormError(true);
       return;
     }
-
-    this.setState({ formError: false });
+    setFormError(false);
 
     const formattedFormData = {
-      ...this.state,
-      taskError: null,
-      formError: null
+      task: editTask
     };
 
-    fetch(`${BASE_URL}/todo/${this.props.item.id}.json`, {
-      method: "PUT",
+    fetch(`${BASE_URL}/todo/${item.id}.json`, {
+      method: "PATCH",
       body: JSON.stringify(formattedFormData)
     }).then(() => {
-      this.props.onSave();
+      props.onSave();
     });
   };
 
-  handleOnCloseClick = () => {
-    this.props.onClose();
+  const handleOnCloseClick = () => {
+    props.onClose();
   };
 
-  render() {
-    const { item } = this.props;
-    const { task, taskError } = this.state;
+  return (
+    <div key={item.id}>
+      <List.Item>
+        <Form.Field>
+          <Input
+            fluid
+            size="small"
+            type="text"
+            value={editTask}
+            name="task"
+            onChange={handleOnChange}
+          />
+          {taskError ? (
+            <Message negative>
+              <Message.Header>Incorrect input</Message.Header>
+              <p>The task should be between 5 and 35 characters</p>
+            </Message>
+          ) : null}
+        </Form.Field>
+      </List.Item>
+      <Button
+        size="small"
+        attached="left"
+        positive
+        icon
+        onClick={handleOnSaveClick}
+        disabled={!item.task}
+      >
+        <Icon name="save" />
+      </Button>
+      <Button size="small" attached="right" icon onClick={handleOnCloseClick}>
+        <Icon name="close" />
+      </Button>
+      <Checkbox done={item.done} id={item.id} item={item.task} />
+      <hr />
+    </div>
+  );
+};
 
-    return (
-      <div key={item.id}>
-        <List.Item>
-          <Form.Field>
-            <Input
-              fluid
-              size='small'
-              type="text"
-              value={task}
-              name="task"
-              onChange={this.handleOnChange}
-            />
-            {taskError ? (
-              <Message negative>
-                <Message.Header>Incorrect input</Message.Header>
-                <p>The task should be between 5 and 35 characters</p>
-              </Message>
-            ) : null}
-          </Form.Field>
-        </List.Item>
-        <Button
-          size="small"
-          attached="left"
-          positive
-          icon
-          onClick={this.handleOnSaveClick}
-          disabled={!task}
-        >
-          <Icon name="save" />
-        </Button>
-        <Button
-          size="small"
-          attached="right"
-          icon
-          onClick={this.handleOnCloseClick}
-        >
-          <Icon name="close" />
-        </Button>
-        <Checkbox done={item.done} id={item.id} item={item.task} />
-        <hr />
-      </div>
-    );
-  }
-}
+export default ToDoEditList;
+
+// export default class ToDoEditList extends React.Component {
+//   state = {
+//     ...this.props.item,
+//     id: null,
+//     taskError: false,
+//     formError: false
+//   };
+
+//   handleOnChange = event => {
+//     this.setState({
+//       [event.target.name]: event.target.value
+//     });
+//   };
+
+//   handleOnSaveClick = event => {
+//     event.preventDefault();
+
+//     let error = false;
+
+//     const taskLength = this.state.task.length;
+
+//     if (taskLength < 5 || taskLength > 35) {
+//       this.setState({ taskError: true });
+//       error = true;
+//     } else {
+//       this.setState({ taskError: false });
+//     }
+
+//     if (error) {
+//       this.setState({ formError: true });
+//       return;
+//     }
+
+//     this.setState({ formError: false });
+
+//     const formattedFormData = {
+//       ...this.state,
+//       taskError: null,
+//       formError: null
+//     };
+
+//     fetch(`${BASE_URL}/todo/${this.props.item.id}.json`, {
+//       method: "PUT",
+//       body: JSON.stringify(formattedFormData)
+//     }).then(() => {
+//       this.props.onSave();
+//     });
+//   };
+
+//   handleOnCloseClick = () => {
+//     this.props.onClose();
+//   };
+
+//   render() {
+//     const { item } = this.props;
+//     const { task, taskError } = this.state;
+
+//     return (
+//       <div key={item.id}>
+//         <List.Item>
+//           <Form.Field>
+//             <Input
+//               fluid
+//               size='small'
+//               type="text"
+//               value={task}
+//               name="task"
+//               onChange={this.handleOnChange}
+//             />
+//             {taskError ? (
+//               <Message negative>
+//                 <Message.Header>Incorrect input</Message.Header>
+//                 <p>The task should be between 5 and 35 characters</p>
+//               </Message>
+//             ) : null}
+//           </Form.Field>
+//         </List.Item>
+//         <Button
+//           size="small"
+//           attached="left"
+//           positive
+//           icon
+//           onClick={this.handleOnSaveClick}
+//           disabled={!task}
+//         >
+//           <Icon name="save" />
+//         </Button>
+//         <Button
+//           size="small"
+//           attached="right"
+//           icon
+//           onClick={this.handleOnCloseClick}
+//         >
+//           <Icon name="close" />
+//         </Button>
+//         <Checkbox done={item.done} id={item.id} item={item.task} />
+//         <hr />
+//       </div>
+//     );
+//   }
+// }
