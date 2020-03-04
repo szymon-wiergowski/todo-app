bdaf1fbb59cd9b101f5a78d06a788ba598ba6ea2
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-03-04 22:02:56 +0100

    add hooks to Navigation.js

--- todo-app/src/navigation/Navigation.js
+++ todo-app/src/navigation/Navigation.js
@@ -8,84 +8,42 @@ import FirebaseConfig from "../firebase/Firebase";
 
 firebase.initializeApp(FirebaseConfig);
 
-// const Navigation = () => {
-//   const [user, setUser] = useState(null);
+const Navigation = () => {
+  const [user, setUser] = useState(null);
 
-//   const handleSignOut = () => {
-//     firebase.auth().signOut();
-//   };
-
-//   useEffect(() => {
-//     firebase.auth().onAuthStateChanged(user => {
-//       setUser(user);
-//     }, []);
-//   });
-
-//   return (
-//     <Menu inverted>
-//       <Link to="/">
-//         <Menu.Item name="home">Home</Menu.Item>
-//       </Link>
-//       <Link to="/todo">
-//         <Menu.Item name="todo">ToDo App</Menu.Item>
-//       </Link>
-//       <Menu.Menu position="right">
-//         {!!user ? (
-//           <>
-//             <Button inverted onClick={handleSignOut}>
-//               Sign out
-//             </Button>
-//           </>
-//         ) : (
-//           <Link to="/sign-in">
-//             <Menu.Item name="Sign in" />
-//           </Link>
-//         )}
-//       </Menu.Menu>
-//     </Menu>
-//   );
-// };
-
-// export default Navigation;
-
-export default class Navigation extends React.Component {
-  state = {
-    user: null
-  };
-
-  handleSignOut = () => {
+  const handleSignOut = () => {
     firebase.auth().signOut();
   };
 
-  componentDidMount() {
+  useEffect(() => {
     firebase.auth().onAuthStateChanged(user => {
-      this.setState({ user });
+      setUser(user);
     });
-  }
+  });
 
-  render() {
-    return (
-      <Menu inverted>
-        <Link to="/">
-          <Menu.Item name="home">Home</Menu.Item>
-        </Link>
-        <Link to="/todo">
-          <Menu.Item name="todo">ToDo App</Menu.Item>
-        </Link>
-        <Menu.Menu position="right">
-          {!!this.state.user ? (
-            <>
-              <Button inverted onClick={this.handleSignOut}>
-                Sign out
-              </Button>
-            </>
-          ) : (
-            <Link to="/sign-in">
-              <Menu.Item name="Sign in" />
-            </Link>
-          )}
-        </Menu.Menu>
-      </Menu>
-    );
-  }
-}
+  return (
+    <Menu inverted>
+      <Link to="/">
+        <Menu.Item name="home">Home</Menu.Item>
+      </Link>
+      <Link to="/todo">
+        <Menu.Item name="todo">ToDo App</Menu.Item>
+      </Link>
+      <Menu.Menu position="right">
+        {!!user ? (
+          <>
+            <Button inverted onClick={handleSignOut}>
+              Sign out
+            </Button>
+          </>
+        ) : (
+          <Link to="/sign-in">
+            <Menu.Item name="Sign in" />
+          </Link>
+        )}
+      </Menu.Menu>
+    </Menu>
+  );
+};
+
+export default Navigation;
