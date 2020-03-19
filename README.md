889bdf6c78bd7e768f4f978c476b5c4ed76987c4
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-03-19 21:53:59 +0100

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-## todo-app
-
-
-Todo application. After registration, the user can add a task, mark that it is done, delete, and modify. (used: React.js, React-Router, Redux, hooks, firebase).
-
-Demo: https://szymon-wiergowski.github.io/todo-app
-
-
---
+++ precache-manifest.256bfa698cd7d9ced5a14030ac447f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7c556c795ba6120ee6139e4ac24edf",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "5448e2f6508a95c884b3",
+    "url": "/todo-app/static/css/main.4f033ccc.chunk.css"
+  },
+  {
+    "revision": "f605f972dd5b949b4332",
+    "url": "/todo-app/static/js/2.d8be3f6c.chunk.js"
+  },
+  {
+    "revision": "0010839e1b2e4ad16e8218a40aecd67e",
+    "url": "/todo-app/static/js/2.d8be3f6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5448e2f6508a95c884b3",
+    "url": "/todo-app/static/js/main.be4eb9a1.chunk.js"
+  },
+  {
+    "revision": "1ce3a5f9a054516a4785",
+    "url": "/todo-app/static/js/runtime-main.344e3758.js"
+  }
+]);
