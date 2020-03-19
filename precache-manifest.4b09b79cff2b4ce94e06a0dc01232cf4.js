c68ce821e579ff9edc1ff62e51cd4f6f38a6273b
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-03-19 20:38:57 +0100

    merge with master

---
+++ precache-manifest.4b09b79cff2b4ce94e06a0dc01232cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987891fe99e5f84c403bd1187136f7c0",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "9824bd0ea8d71d9e3934",
+    "url": "/todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "415c41c6039cf681c944",
+    "url": "/todo-app/static/js/2.df5d9b52.chunk.js"
+  },
+  {
+    "revision": "0010839e1b2e4ad16e8218a40aecd67e",
+    "url": "/todo-app/static/js/2.df5d9b52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9824bd0ea8d71d9e3934",
+    "url": "/todo-app/static/js/main.c091ce52.chunk.js"
+  },
+  {
+    "revision": "1ce3a5f9a054516a4785",
+    "url": "/todo-app/static/js/runtime-main.344e3758.js"
+  }
+]);
